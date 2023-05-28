5030545fa700d29ebe9cbf30410cd38189d300b0
Ivan Gorokhov <xgorokhovx@gmail.com>
2023-05-29 01:09:23 +0300

    init: name-badge using TypeScript

---
+++ src/components/application.tsx
@@ -0,0 +1,10 @@
+// import ControlPanel from './controls';
+import NameBadge from './name-badge';
+
+const Application = () => (
+  <main className="application">
+    <NameBadge name={'Ivan'} />
+  </main>
+);
+
+export default Application;
